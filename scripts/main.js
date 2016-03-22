e938fa67cfe112c89224d296f36efd08fcf0012b
anna hunter <annfmonty@gmail.com>
2016-03-22 08:26:11 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var uName = $("#userName");
+var uPassword = $("#passWord");
+
+var user = [
+	{
+		name: "aaron@theironyard.com",
+		password: "password123",
+	 },
+ 	{
+ 		name: "admin@google.com",
+ 		password: "pandas" | "honeycrisp",
+	 }];
+console.log(user [1.1]);
+
+// $(".signIn").on("click", function()){
+// 	if (uName.va() === user[0,0]){
+
+// 	}if(uPassword.val()=== user [0.1])
+// }
+
+
